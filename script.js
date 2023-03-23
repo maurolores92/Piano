b143f80544a7698729466c73106ce0d69ff11774
Mauricio <maurolores1992@gmail.com>
2023-03-23 03:54:13 -0300

    modificando

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-  
-    function empezarSonido(e) {
-      const audio = document.querySelector(`audio[data-sonido="${e.keyCode}"]`);
-      const key = document.querySelector(`.key[data-sonido="${e.keyCode}"]`);
-      if(!audio) return;  // detiene la funcion para que no se ejecuten todos juntos
-      audio.currentTime = 0; //Retroceder hasta el principio
-      audio.play()
-      key.classList.add('playing');
-    }
+const keys = document.querySelectorAll('.key');
 
-function removeTransition(e) {
+function empezarSonido(e) {
+  const audio = document.querySelector(`audio[data-sonido="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-sonido="${e.keyCode}"]`);
+  if(!audio) return;  // detiene la funcion para que no se ejecuten todos juntos
+  audio.currentTime = 0; //Retroceder hasta el principio
+  audio.play()
+  key.classList.add('playing');
+  }
+
+function eliminarTransicion(e) {
   if(e.propertyName !== 'transform') return; //saltalo si no es una transformacion
   this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
+keys.forEach(key => key.addEventListener('transitionend', eliminarTransicion));
 window.addEventListener('keydown', empezarSonido);
